5d05246bb95d90c8349b9effc16a1a09d95f12e2
Michael Wood <aloneibreak83@googlemail.com>
2017-10-14 23:12:14 +0100

    Update cards.js

--- generator/js/cards.js
+++ generator/js/cards.js
@@ -360,6 +360,9 @@ function card_generate_back(data, options) {
     console.log('options.rounded_corners', options.rounded_corners);
     result += '<div class="card card-size-' + options.card_size + ' ' + (options.rounded_corners ? 'rounded-corners' : '') + '" ' + style_color + '>';
     result += '  <div class="card-back" ' + background_style + '>';
+	if(data.bgtext && data.bgtext != "") {
+		result += '     <div>' + data.bgtext + '</div>';
+	}
 	if (!url)
 	{
 		result += '    <div class="card-back-inner">';
